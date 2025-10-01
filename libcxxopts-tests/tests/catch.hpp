06989f2d098fc59cbb663030ac34019e42478c7c
lyrahgames <lyrahgames@mailbox.org>
2025-10-01 23:36:04 +0200

    Move library tests to external package
    This is done to remove the in-place copy of Catch2.
    Instead, we depend on the external `cppget.org` package.

---
+++ libcxxopts-tests/tests/catch.hpp
@@ -0,0 +1,5 @@
+// This header allows upstream sources to include Catch2
+// with `#include "catch.hpp"`. Internally it forwards to
+// Catch2's official inclusion scheme:
+//
+#include <catch2/catch.hpp>
